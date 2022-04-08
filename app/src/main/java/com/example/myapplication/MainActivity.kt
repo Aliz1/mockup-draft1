9545281b3f3e5e689d105197f0a0c38d2adfbc02
Aliz1 <72601502+Aliz1@users.noreply.github.com>
2022-04-08 14:18:11 +0200

    Added function to show alternative trip based on radio button. Updated GUI.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,8 +3,13 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.RadioButton
+import java.lang.reflect.Type
+import java.security.acl.Group
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +19,22 @@ class MainActivity : AppCompatActivity() {
         buttonClick.setOnClickListener {
             val intent = Intent(this, NewActivity::class.java)
             startActivity(intent)
+        }
+        val radioButtonClick = findViewById<RadioButton>(R.id.Sit)
+        radioButtonClick.setOnClickListener{
+            val group = findViewById<View>(R.id.group)
+            val group2 = findViewById<View>(R.id.group2)
+            group.visibility = View.INVISIBLE;
+            group2.visibility = View.VISIBLE;
+
+        }
+
+        val radioButtonClickChange = findViewById<RadioButton>(R.id.stigAv)
+        radioButtonClickChange.setOnClickListener{
+            val group = findViewById<View>(R.id.group)
+            val group2 = findViewById<View>(R.id.group2)
+            group.visibility = View.VISIBLE;
+            group2.visibility = View.INVISIBLE;
 
         }
     }
